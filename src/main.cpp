2e6277d2f9a122eb77d4e382003f5f1feb8dd04d
kkosteck <k.kostecki99@gmail.com>
2019-03-12 20:12:28 +0100

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
+class Numbers{
+	public:
+		int firstNum;
+		int secondNum;
+		Numbers(string firstInput, string secondInput);
+		string operator+(int firstNum, int secondNum);
+};
+
+string operator+(int firstNum, int secondNum){
+		
+}
+
+Numbers::Numbers(string firstInput, string secondInput){
+	cout<<"Input first number: ";
+	cin>>firstInput;
+	cout<<"Input second number: ";
+	cin>>secondInput;
+	firstNum = strtol(&firstInput[0], NULL, 16);
+	secondNum = strtol(&secondInput[0], NULL, 16);
+}
+
+
 int main(){
-  int a,b;
-  cout<<"First number:";
-  cin>>a;
-  cout<<"Second number:";
-  cin>>b;
-  cout<<"Result: "<<a+b<<endl;
+	string firstInput, secondInput;
+	char operation;
+	Numbers input(firstInput, secondInput);
+	cout<<"Choose operation (+, -, /, *): ";
+	cin>>operation;
 }
